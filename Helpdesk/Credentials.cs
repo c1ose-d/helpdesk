26781510ec8a0f1924158ca91f252626dab20ccf
PleshkovU <128600599+PleshkovU@users.noreply.github.com>
2024-01-30 14:15:39 +0300

    30-01-2024 14:15

---
+++ Helpdesk/Credentials.cs
@@ -0,0 +1,9 @@
+﻿namespace Helpdesk;
+
+public static class Credentials
+{
+    public static bool SignedIn { get; set; } = false;
+
+    public static string UserName { get; } = "gmn";
+    public static string Password { get; } = "xEgcZG1w";
+}
